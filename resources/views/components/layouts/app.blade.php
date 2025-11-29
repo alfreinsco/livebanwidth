e379442b8d265dae9808c4ee6290a6acc9bea361
marthinsalakory <marthinsalakory11@gmail.com>
2025-11-29 14:53:21 +0900

    Menambahkan daisyui dan fontawesome

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,10 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ config('app.name', 'LiveBanwidth') }}</title>
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
 </head>
 
 <body>
     {{ $slot }}
+
+    <script src="{{ asset('fontawesome/js/all.min.js') }}"></script>
 </body>
 
 </html>
